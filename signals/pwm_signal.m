32e6f86eee2133c7c3d707ba60db0e24f80bdd66
Bruno <brunolnetto@gmail.com>
2020-02-26 16:50:06 -0300

    PWM Signal

---
+++ signals/pwm_signal.m
@@ -0,0 +1,18 @@
+function val = pwm_signal(t, max_val, min_val, alpha, T)
+    persistent t0;
+    if(isempty(t0))
+        t0 = 0;
+    end
+    
+    if(t - t0 > T)
+        t0 = t;
+    end
+
+    t_i = t - t0;
+    
+    if(t_i < alpha*T)
+        val = max_val;
+    else
+        val = min_val;
+    end
+end
