bc7ba7533d3659e4702e5c093bd2a562af4e1b8d
Bruno Peixoto <brunolnetto@gmail.com>
2020-03-13 15:22:05 -0300

    Utilitaries

--- basics/uniform.m
+++ basics/uniform.m
@@ -1,6 +1,11 @@
 function z = uniform(a, b, dim)
-    n = prod(dim);
-    z = zeros(n, 1);
+    if(nargin == 2)
+        dim = 1;
+        n = 1
+    else
+        n = prod(dim);
+        z = zeros(n, 1);
+    end
     
     for i = 1:n
         z(i) = a + (b-a)*rand();
